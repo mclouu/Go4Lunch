4c983f40707387672394529a7e0dfc4e3e143d5e
Romain Mathieu <rmathieu13@gmail.com>
2023-03-14 07:53:14 +0100

    Swith for changing theme

--- app/src/main/java/com/roms/go4lunch/ui/setting/SettingsFragment.java
+++ app/src/main/java/com/roms/go4lunch/ui/setting/SettingsFragment.java
@@ -2,14 +2,33 @@ package com.roms.go4lunch.ui.setting;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
 
 import com.roms.go4lunch.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
+
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        SwitchPreference switchTheme = findPreference("Theme");
+
+
+        boolean a = switchTheme.isChecked();
+
+
+        if (a){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
     }
+
+
 }
