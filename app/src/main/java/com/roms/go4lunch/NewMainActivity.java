4cc8ecd4fe89e06a66eb82bb49eb87af81b36976
Romain Mathieu <rmathieu13@gmail.com>
2023-03-14 02:09:58 +0100

    Create new Mainactivity with Navigation UI

---
+++ app/src/main/java/com/roms/go4lunch/NewMainActivity.java
@@ -0,0 +1,64 @@
+package com.roms.go4lunch;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+import com.roms.go4lunch.databinding.ActivityMainBinding;
+
+import java.util.Objects;
+
+public class NewMainActivity extends BaseActivity<ActivityMainBinding> {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    ActivityMainBinding getViewBinding() {
+        return ActivityMainBinding.inflate(getLayoutInflater());
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toolbar toolbar = binding.appBarMain.toolbar;
+        DrawerLayout drawer = binding.activityMainDrawerLayout;
+        NavigationView navigationView = binding.drawerNavigation;
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
+
+//        R.id.nav_lunch, R.id.nav_setting
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                navController.getGraph())
+                .setOpenableLayout(drawer)
+                .build();
+
+        setSupportActionBar(toolbar);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
